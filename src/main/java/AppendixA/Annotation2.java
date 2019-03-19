b3ec212e6a9c91ccb35329a8dafe3753c253f00d
simjunbo <s860620@gmail.com>
2019-03-19 11:32:22 +0900

    change

--- src/main/java/AppendixA/Annotation2.java
+++ src/main/java/AppendixA/Annotation2.java
@@ -1,13 +1,10 @@
 package AppendixA;
 
-import com.sun.istack.internal.NotNull;
-
 // 형식 어노테이션
 public class Annotation2 {
 
     Person person = new Person("simjunbo");
 
-    @NotNull
     String name = person.getName();
 
     //List<@NotNull Person> persions = new ArrayList<>();
